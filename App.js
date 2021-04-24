9abc07725245f2c1504b6ea301238f30a8271c36
Umer Ziyad Satti <umersatti919@gmail.com>
2021-04-25 02:25:16 +0500

    First Commit

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';    
+import LoginScreen from './screens/LoginScreen';
+import { createStackNavigator} from '@react-navigation/stack';
+import RegisterScreen from './screens/RegisterScreen';
+
+const Stack = createStackNavigator();
+
+const globalScreenOptions = {
+
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white"
+
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+      <Stack.Screen name="Login to your Account" component={LoginScreen} /> 
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer> 
+    
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBIkxudCIgk91G4EVCLd6vzgZG3UcLZmlc",
+    authDomain: "signal-clone-yt-build-53ce1.firebaseapp.com",
+    projectId: "signal-clone-yt-build-53ce1",
+    storageBucket: "signal-clone-yt-build-53ce1.appspot.com",
+    messagingSenderId: "458888150789",
+    appId: "1:458888150789:web:cf64454361c9f52028016c"
+  };
